af27bf6556adb21276a29fd772ce53c95ac743ab
JeffGuo576 <jeffreyguo315@gmail.com>
2023-10-31 12:12:22 -0400

    leave done maybe, need to get a random word

--- townService/src/town/games/HangManGame.ts
+++ townService/src/town/games/HangManGame.ts
@@ -1,9 +1,12 @@
+import { readFileSync } from 'fs';
 import Player from '../../lib/Player';
 import Game from './Game';
 import { GameMove, HangManGameState, HangManMove } from '../../types/CoveyTownSocket';
 import InvalidParametersError, {
   GAME_FULL_MESSAGE,
+  GAME_NOT_IN_PROGRESS_MESSAGE,
   PLAYER_ALREADY_IN_GAME_MESSAGE,
+  PLAYER_NOT_IN_GAME_MESSAGE,
 } from '../../lib/InvalidParametersError';
 
 export default class HangManGame extends Game<HangManGameState, HangManMove> {
@@ -17,7 +20,14 @@ export default class HangManGame extends Game<HangManGameState, HangManMove> {
   }
 
   public applyMove(move: GameMove<HangManMove>): void {
-    throw new Error('Method not implemented.');
+    if (this.state.status !== 'IN_PROGRESS') {
+      throw new InvalidParametersError(GAME_NOT_IN_PROGRESS_MESSAGE);
+    }
+  }
+
+  private _getRandomWordFromFile(filePath: string): string {
+    const file = readFileSync('/words.txt', 'utf-8');
+    return '';
   }
 
   /**
@@ -33,6 +43,7 @@ export default class HangManGame extends Game<HangManGameState, HangManMove> {
     if (this._players.includes(player)) {
       throw new InvalidParametersError(PLAYER_ALREADY_IN_GAME_MESSAGE);
     }
+
     // Case for the first player
     if (this._players.length === 0) {
       this.state = {
@@ -73,6 +84,15 @@ export default class HangManGame extends Game<HangManGameState, HangManMove> {
   }
 
   protected _leave(player: Player): void {
-    throw new Error('Method not implemented.');
+    if (!this._players.includes(player)) {
+      throw new InvalidParametersError(PLAYER_NOT_IN_GAME_MESSAGE);
+    }
+
+    // If the game has no player then we reset the states
+    if (this._players.length === 1) {
+      this.state.status = 'OVER';
+      this.state.winner = undefined;
+      this.state.mistakes = 9;
+    }
   }
 }
