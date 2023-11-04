d29a66df77bb05d64c9d63eddf80d498bfaa6538
JeffGuo576 <jeffreyguo315@gmail.com>
2023-11-04 11:35:56 -0400

    current turn works properly

--- shared/types/CoveyTownSocket.d.ts
+++ shared/types/CoveyTownSocket.d.ts
@@ -18,9 +18,9 @@ export type TownJoinResponse = {
 }
 
 export interface HangManMove {
-  letterGuess?: 'A' | 'B' | 'C' | 'D' | 'E' |
-  'F' | 'G' | 'H' | 'I' | 'J' | 'K' | 'L' | 'M'| 'O' |
-  'P' | 'Q' | 'R' | 'S' | 'T' | 'U'| 'V' | 'W' | 'X' | 'Y' | 'Z',
+  letterGuess?: 'a' | 'b' | 'c' | 'd' | 'e' |
+  'f' | 'g' | 'h' | 'i' | 'j' | 'k' | 'l' | 'm'| 'n' | 'o' |
+  'p' | 'q' | 'r' | 's' | 't' | 'u'| 'v' | 'w' | 'x' | 'y' | 'z',
   wordGuess?: string,
   playerID?: PlayerID
 }
