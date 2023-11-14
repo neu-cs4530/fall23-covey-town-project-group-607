f7b52d980ad653f860bdc298d8ff6cf943acb506
Meng Un <un.me@northeastern.edu>
2023-11-14 18:42:49 -0500

    Fixed linting issue

--- frontend/src/components/Town/interactables/Hangman/HangmanComponent.tsx
+++ frontend/src/components/Town/interactables/Hangman/HangmanComponent.tsx
@@ -10,68 +10,64 @@ const Hangman: React.FC<HangmanProps> = ({ mistakeCount }) => {
     <VStack spacing={2}>
       {/* Ground Base */}
       {mistakeCount > 0 && (
-        <Box height="20px">
-          <Text fontSize="2xl">___</Text>
+        <Box height='20px'>
+          <Text fontSize='2xl'>___</Text>
         </Box>
       )}
       {/* Post */}
       {mistakeCount > 1 && (
-        <Box height="60px">
-          <Text fontSize="2xl">|</Text>
+        <Box height='60px'>
+          <Text fontSize='2xl'>|</Text>
         </Box>
       )}
       {/* Support Brace */}
       {mistakeCount > 2 && (
         <HStack>
-          <Box width="20px">
-            <Text fontSize="2xl">/</Text>
+          <Box width='20px'>
+            <Text fontSize='2xl'>/</Text>
           </Box>
         </HStack>
       )}
       {/* Long Brace (Extending Beam) */}
       {mistakeCount > 3 && (
-        <Box height="20px">
-          <Text fontSize="2xl">-</Text>
+        <Box height='20px'>
+          <Text fontSize='2xl'>-</Text>
         </Box>
       )}
       {/* Rope */}
       {mistakeCount > 4 && (
-        <Box height="20px">
-          <Text fontSize="2xl">|</Text>
+        <Box height='20px'>
+          <Text fontSize='2xl'>|</Text>
         </Box>
       )}
       {/* Head */}
       {mistakeCount > 5 && (
-        <Box height="20px">
-          <Circle size="10px" bg="black" />
+        <Box height='20px'>
+          <Circle size='10px' bg='black' />
         </Box>
       )}
       {/* Body */}
       {mistakeCount > 6 && (
-        <Box height="50px">
-          <Text fontSize="2xl">|</Text>
+        <Box height='50px'>
+          <Text fontSize='2xl'>|</Text>
         </Box>
       )}
       {/* Arms */}
       {mistakeCount > 7 && (
         <HStack>
-          <Box width="20px">
-            <Text fontSize="2xl">/</Text>
+          <Box width='20px'>
+            <Text fontSize='2xl'>/</Text>
           </Box>
-          <Box width="20px">
-            {mistakeCount > 8 && <Text fontSize="2xl">\</Text>}
-          </Box>
+          <Box width='20px'>{mistakeCount > 8 && <Text fontSize='2xl'>\</Text>}</Box>
         </HStack>
       )}
       {/* Legs */}
       {mistakeCount > 9 && (
         <HStack>
-          <Box width="20px">
-            <Text fontSize="2xl">/</Text>
+          <Box width='20px'>
+            <Text fontSize='2xl'>/</Text>
           </Box>
-          <Box width="20px">
-            {mistakeCount > 10 && <Text fontSize="2xl">\</Text>}
-          </Box>
+          <Box width='20px'>{mistakeCount > 10 && <Text fontSize='2xl'>\</Text>}</Box>
         </HStack>
       )}
     </VStack>
@@ -79,4 +75,3 @@ const Hangman: React.FC<HangmanProps> = ({ mistakeCount }) => {
 };
 
 export default Hangman;
-
